15b83d8a0d33f6a3fd8a7f89c48a0bd801deb6e5
Enjamul Islam <mdenjamulislam527@gmail.com>
2024-02-07 22:45:37 +0600

    Email Varification, and Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,38 +1,65 @@
-import { signInWithEmailAndPassword } from "firebase/auth"
-import React, { useState } from "react"
-import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5"
-import auth from "../../firebase/firebase.config"
+import { sendPasswordResetEmail, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useRef, useState } from "react";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import auth from "../../firebase/firebase.config";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    const [showPassword, setShowPassword] = useState()
-    const [errorMessage, setErrorMessage] = useState()
-    const [loginSuccess, setLoginSuccess] = useState()
+    const [showPassword, setShowPassword] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+    const [loginSuccess, setLoginSuccess] = useState();
+
+    const emailRef = useRef(null);
 
     const handleLogin = (e) => {
-        e.preventDefault()
-        const email = e.target.email.value
-        const password = e.target.pass.value
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.pass.value;
 
-        console.log(email, password)
+        console.log(email, password);
         // reser error & success message
-        setRegisterError("")
-        setRegisterSuccess("")
+        setErrorMessage("");
+        setLoginSuccess("");
 
         signInWithEmailAndPassword(auth, email, password)
             .then((result) => {
-                const user = result.user
-                setLoginSuccess("Login Successfully!")
+                const user = result.user;
+                if (result.user.emailVerified) {
+                    setLoginSuccess("Login Successfully!");
+                } else {
+                    alert("Please check your email and verify.");
+                }
             })
             .catch((error) => {
-                setErrorMessage(error.message)
+                setErrorMessage(error.message);
+            });
+    };
+
+    const handleForgetPass = () => {
+        const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const email = emailRef.current.value;
+        if (!email) {
+            console.log("Please rpovide an email address.");
+            return;
+        } else if (!emailRegex.test(email)) {
+            console.log("Please provide a valid email.");
+            return;
+        }
+
+        sendPasswordResetEmail(auth, email)
+            .then((result) => {
+                setLoginSuccess("Recovery Email send Successfully!");
             })
-    }
+            .catch((error) => {
+                setErrorMessage(error.message);
+            });
+    };
     return (
         <section className="w-full mt-10 flex items-center justify-center">
             <div className="w-full md:w-[480px] mx-auto p-4 bg-slate-800 shadow-md rounded-2xl">
                 <h2 className="text-2xl font-semibold text-center pb-5">Login</h2>
                 <form onSubmit={handleLogin} className="flex flex-col gap-3">
-                    <input type="email" name="email" placeholder="Type here" required className="input input-bordered input-success w-full" />
+                    <input ref={emailRef} type="email" name="email" placeholder="Type email" required className="input input-bordered input-success w-full" />
                     <div className="flex items-center relative">
                         <input type={showPassword ? "text" : "password"} name="pass" placeholder="Password" required className="input input-bordered input-success w-full" />
 
@@ -48,20 +75,21 @@ const Login = () => {
                             </label>
                         </div>
                         <div className="form-control">
-                            <a href="#">Forget Password</a>
+                            <a onClick={handleForgetPass} className="cursor-pointer">
+                                Forget Password
+                            </a>
                         </div>
                     </div>
                     <button className="btn btn-primary">Login</button>
+                    <p>
+                        Don't have any account? <Link to="/register">Register Now</Link>
+                    </p>
                 </form>
-                {
-                    errorMessage && <p className="text-warning">{errorMessage}</p>
-                }
-                {
-                    loginSuccess && <p className="text-green-600">{loginSuccess}</p>
-                }
+                {errorMessage && <p className="text-warning">{errorMessage}</p>}
+                {loginSuccess && <p className="text-green-600">{loginSuccess}</p>}
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,30 +1,31 @@
-import { createUserWithEmailAndPassword } from "firebase/auth"
-import auth from "../../firebase/firebase.config"
-import { useState } from "react"
-import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5"
+import { createUserWithEmailAndPassword, sendEmailVerification } from "firebase/auth";
+import auth from "../../firebase/firebase.config";
+import { useState } from "react";
+import { IoEyeOutline, IoEyeOffOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    const [registerError, setRegisterError] = useState("")
-    const [registerSuccess, setRegisterSuccess] = useState("")
-    const [showPassword, setShowPassword] = useState(false)
+    const [registerError, setRegisterError] = useState("");
+    const [registerSuccess, setRegisterSuccess] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleRegister = (e) => {
-        e.preventDefault()
-        const email = e.target.email.value
-        const password = e.target.pass.value
+        e.preventDefault();
+        const email = e.target.email.value;
+        const password = e.target.pass.value;
         const accepted = e.target.terms.checked;
-        console.log(email, password, accepted)
+        console.log(email, password, accepted);
 
         // reser error & success message
-        setRegisterError("")
-        setRegisterSuccess("")
+        setRegisterError("");
+        setRegisterSuccess("");
 
         if (password.length < 6) {
-            setRegisterSuccess("Password should be at leasr 6 characters or longer")
-            return
+            setRegisterSuccess("Password should be at leasr 6 characters or longer");
+            return;
         } else if (!/[A-Z]/.test(password)) {
-            setRegisterError("Your password not full-fill our requirement")
-            return
+            setRegisterError("Your password not full-fill our requirement");
+            return;
         } else if (!accepted) {
             setRegisterError("Please accepted our terms and conditions.");
             return;
@@ -33,27 +34,30 @@ const Register = () => {
         // Create user
         createUserWithEmailAndPassword(auth, email, password)
             .then((result) => {
-                console.log(result.user)
-                setRegisterSuccess("Registration Successfully!")
+                console.log(result.user);
+                setRegisterSuccess("Registration Successfully!");
+
+                sendEmailVerification(result.user).then(() => {
+                    alert("Verification email send to ", email);
+                });
             })
             .catch((error) => {
-                console.log(error)
-                setRegisterError(error.message)
-            })
-    }
+                console.log(error);
+                setRegisterError(error.message);
+            });
+    };
     return (
         <section className="w-full mt-10 flex items-center justify-center">
             <div className="w-full md:w-[480px] mx-auto p-4 bg-slate-800 shadow-md rounded-2xl">
                 <h2 className="text-2xl font-semibold text-center pb-5">Registration</h2>
                 <form onSubmit={handleRegister} className="flex flex-col gap-3">
-                    <input type="email" name="email" placeholder="Type here" required className="input input-bordered input-success w-full" />
+                    <input type="text" name="name" placeholder="Type your name" required className="input input-bordered input-success w-full" />
+                    <input type="email" name="email" placeholder="Type your email" required className="input input-bordered input-success w-full" />
                     <div className="flex items-center relative">
                         <input type={showPassword ? "text" : "password"} name="pass" placeholder="Password" required className="input input-bordered input-success w-full" />
-                        
+
                         <button className="absolute right-2" onClick={() => setShowPassword(!showPassword)}>
-                            {
-                                showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />
-                            }
+                            {showPassword ? <IoEyeOffOutline /> : <IoEyeOutline />}
                         </button>
                     </div>
                     <div className="form-control">
@@ -63,12 +67,15 @@ const Register = () => {
                         </label>
                     </div>
                     <button className="btn btn-success">Register</button>
+                    <p>
+                        I have an account. <Link to="/login">Login</Link>
+                    </p>
                 </form>
                 {registerError && <p className="text-red-500">{registerError}</p>}
                 {registerSuccess && <p className="text-green-600">{registerSuccess}</p>}
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Register
+export default Register;
